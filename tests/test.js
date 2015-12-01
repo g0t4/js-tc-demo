94a4dc6d07fd499c30ac4acc99c29bfd07e6a72e
Wes Higbee <wes.mcclure@gmail.com>
2015-12-01 18:05:51 -0500

    Add karma sample tests

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+describe("Some application tests", function(){
+  it("Should be true", function(){
+    expect(true);
+  });
+
+  it("Should also be true", function(){
+    expect(true);
+  });
+});
