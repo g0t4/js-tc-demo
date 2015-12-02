f8503fe2b5f944de094168c99008082f6eac7b59
Wes Higbee <wes.mcclure@gmail.com>
2015-12-02 10:56:47 -0500

    change messaage

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import Hello from "./hello.ts";
 
-let hello = new Hello("World");
+let hello = new Hello("World!!!!");
 
 $("body").append(`greeting: ${hello.greeting()}`);
